4de34fa8b2920f8b8c33b3d492b01e0d8d0b399b
chakrigogula <65495204+chakrigogula@users.noreply.github.com>
2020-06-21 18:39:42 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"chakrigogula@rocketmail.com\"\n\
+email = \"vishalsiram50@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
